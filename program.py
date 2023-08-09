b5216f1d7834c0bcd4603dc334620627dc247a3d
Jakrawit.C <mr.jakrawit@gmail.com>
2023-08-09 20:26:56 +0700

    Update program.py

--- program.py
+++ program.py
@@ -2,3 +2,17 @@ print("My first program")
 print("Hello World")
 print("Say Goodbye")
 print("bye bye")
+<<<<<<< HEAD
+=======
+for i in range(1,100):
+    count = 0
+    for j in range(1,i+1):
+        if i%j == 0:
+            count += 1
+        else:
+            pass
+    if count == 2:
+        print(i)
+    else:
+        pass
+>>>>>>> parent of 1968f30 (Revise condition 100 to 50 program.py)

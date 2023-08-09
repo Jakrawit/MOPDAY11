1968f302dc953c8c691f095f6ce4a65e25e279ea
Jakrawit.C <78688967+Jakrawit@users.noreply.github.com>
2023-08-09 20:16:51 +0700

    Revise condition 100 to 50 program.py
    Revise condition 100 to 50

--- program.py
+++ program.py
@@ -2,7 +2,7 @@ print("My first program")
 print("Hello World")
 print("Say Goodbye")
 print("bye bye")
-for i in range(1,100):
+for i in range(1,50):
     count = 0
     for j in range(1,i+1):
         if i%j == 0:

ad173644b1a7e8f6a27046f6a1ce586709bc8a3b
Jakrawit.C <mr.jakrawit@gmail.com>
2023-08-09 20:15:10 +0700

    Added Calculate to find Prime No. -program.py
    Added Calculate to find Prime No. -program.py

--- program.py
+++ program.py
@@ -2,3 +2,14 @@ print("My first program")
 print("Hello World")
 print("Say Goodbye")
 print("bye bye")
+for i in range(1,100):
+    count = 0
+    for j in range(1,i+1):
+        if i%j == 0:
+            count += 1
+        else:
+            pass
+    if count == 2:
+        print(i)
+    else:
+        pass

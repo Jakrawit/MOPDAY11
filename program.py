b69d48407674e7ac66047ea717b375771a5459f4
Jakrawit.C <mr.jakrawit@gmail.com>
2023-08-09 20:22:49 +0700

    Update program.py

--- program.py
+++ program.py
@@ -2,14 +2,4 @@ print("My first program")
 print("Hello World")
 print("Say Goodbye")
 print("bye bye")
-for i in range(1,50):
-    count = 0
-    for j in range(1,i+1):
-        if i%j == 0:
-            count += 1
-        else:
-            pass
-    if count == 2:
-        print(i)
-    else:
-        pass
+
